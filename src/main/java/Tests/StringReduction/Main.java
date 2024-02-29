aaf5d8f5a350964bc4f4252cac78ba1d5db9dfde
Luis Jimenez <luisjavierjn@gmail.com>
2024-02-29 02:02:52 -0500

    fixed format

--- src/main/java/Tests/StringReduction/Main.java
+++ src/main/java/Tests/StringReduction/Main.java
@@ -1,16 +1,14 @@
 package Tests.StringReduction;
 
+//have a function StringChallenge(str) take the str parameter being passed and return the smallest number you can get
+//through the following reduction method. The method is : only the letter a, b and c will be given in str and you must
+//take two different adjacent characters and replace it with the third. for example "ac" can be replaced with "b" but
+//"aa" cannot be replaced with anything. this method is done repeatedly until the string cannot be further reduced,
+//and the length of the resulting string is to be outputted
+//
+//for example if str is "cab" the ca can be reduced to b, and you get bb. the reduction is done so the output should
+//be 2. the result of abcabc is 2, the result of cccc is 4
 public class Main {
-/*
-have a function StringChallenge(str) take the str parameter being passed and return the smallest number you can get
-through the following reduction method. The method is : only the letter a, b and c will be given in str and you must
-take two different adjacent characters and replace it with the third. for example "ac" can be replaced with "b" but
-"aa" cannot be replaced with anything. this method is done repeatedly until the string cannot be further reduced,
-and the length of the resulting string is to be outputted
-
-for example if str is "cab" the ca can be reduced to b, and you get bb. the reduction is done so the output should
-be 2. the result of abcabc is 2, the result of cccc is 4
- */
 
     public static String StringChallenge(String str) {
         if(str == null || str.isEmpty())
