10c3f3f4c162288377ce36fe252ae514ed279b54
Luis Jimenez <luisjavierjn@gmail.com>
2024-03-11 03:29:44 -0500

    added Questions

--- src/main/java/Questions/SumOrConcatenation/Main.java
+++ src/main/java/Questions/SumOrConcatenation/Main.java
@@ -1,9 +1,11 @@
 package Questions.SumOrConcatenation;
 
+// What are the outputs of the following lines?
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(100 + 100 + "Hola");
-        System.out.println("Hola" + 100 + 100);
+        //System.out.println(100 + 100 + "Hola");
+        //System.out.println("Hola" + 100 + 100);
+        //System.out.println("1" + "2" + 1 \* 4.5);
     }
 }
